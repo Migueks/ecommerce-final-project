eba858cb616ccc46a34af3aa445ba2670091ae5d
Migueks <lhlmiguel@gmail.com>
2024-12-16 23:26:20 +0100

    Header design finished

---
+++ components/Header/Header.js
@@ -0,0 +1,47 @@
+import "./Header.css";
+import data from "../../data/data";
+const { logo, title, links, themeDay, themeNight, icons } = data;
+
+export const Header = () => {
+  return `
+        <header>
+                <div>
+                    <a href="/"><img src="${logo.src}" alt="${
+    logo.alt
+  }"></img></a>
+                    <a href="/"><h3>${title}</h3></a>
+                </div>
+                <nav>
+                    <ul class="links">
+                        ${links
+                          .map(
+                            (link) => `
+                            <li>
+                                <a href="${
+                                  link.place
+                                }" id="${link.name.toLowerCase()}-anchor">${
+                              link.name
+                            }</a>
+                            </li>
+                            `
+                          )
+                          .join("")}
+                    </ul>
+                </nav>
+                <nav>
+                    <ul class="icons">
+                        <button id="themeButton" class="themeButton">${themeNight}</button>
+                        ${icons
+                          .map(
+                            (icon) => `
+                            <li>
+                                <img src="${icon.src}" alr="${icon.alt}"></img>
+                            </li>
+                            `
+                          )
+                          .join("")}
+                    </ul>
+                </nav>
+        </header>
+    `;
+};
---
+++ data/data.js
@@ -0,0 +1,43 @@
+const data = {
+  logo: {
+    src: "../img/Logo.png",
+    alt: "Logo de FurniSphere",
+  },
+  title: "FurniSphere",
+  links: [
+    {
+      name: "Home",
+      place: "/",
+    },
+    {
+      name: "Shop",
+      place: "#shop",
+    },
+    {
+      name: "About",
+      place: "#about",
+    },
+    {
+      name: "Contact",
+      place: "#contact",
+    },
+  ],
+  themeDay: "☀",
+  themeNight: "☾",
+  icons: [
+    {
+      src: "../svg/user.svg",
+      alt: "Icono usuario",
+    },
+    {
+      src: "../svg/lupa.svg",
+      alt: "Icono de búsqueda (Lupa)",
+    },
+    {
+      src: "../svg/carrito.svg",
+      alt: "Icono carrito de la compra",
+    },
+  ],
+};
+
+export default data;
